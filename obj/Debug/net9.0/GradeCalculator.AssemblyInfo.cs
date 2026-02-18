98afc810dba7316f63213c1d80e41beeaa68ede0
sigmaBlayne <brc2510@icloud.com>
2026-02-18 18:36:37 -0500

    video
    d

--- obj/Debug/net9.0/GradeCalculator.AssemblyInfo.cs
+++ obj/Debug/net9.0/GradeCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc3bf10a82454e2627a2b75971dda04b201929b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74341faf2e46772318193d88d19f59acefb1f837")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
